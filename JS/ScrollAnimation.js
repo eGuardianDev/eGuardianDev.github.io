bedcda3f7663c14a096f0799247613efa9e2db7b
eGuardianDev <cvetomir200419@abv.bg>
2024-08-13 00:30:35 +0300

    testing new projects layout, logo fixed visisble whne no js

--- JS/ScrollAnimation.js
+++ JS/ScrollAnimation.js
@@ -4,3 +4,24 @@ function changeCss () {
   }
   
   window.addEventListener("scroll", changeCss , false);
+
+
+function SliderProjctesLeft(){
+  console.log("deeznuts");
+  children = document.getElementById('ProjectsDisplayed').children;
+  for(let i = 0; i<children.length;i++){
+    let data =children[i].style.left;
+     data = data.substring(0, data.length - 1);
+     children[i].style.left = (parseInt(data) -25 )+ "%";
+  }
+
+}function SliderProjctesRight(){
+  console.log("deeznuts");
+  children = document.getElementById('ProjectsDisplayed').children;
+  for(let i = 0; i<children.length;i++){
+    let data =children[i].style.left;
+     data = data.substring(0, data.length - 1);
+     children[i].style.left = (parseInt(data) +25 )+ "%";
+  }
+
+}
