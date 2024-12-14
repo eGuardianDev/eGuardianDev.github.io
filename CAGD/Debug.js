116badc218d31c4745940ee84ea3a11646e4ef8b
eGuardianDev <tsvetomirstaikov04@gmail.com>
2024-12-14 22:31:20 +0200

    CAGD Demo

---
+++ CAGD/Debug.js
@@ -0,0 +1,15 @@
+
+const DEBUG = true
+
+function DebugLog(message){
+  if(!DEBUG) return;
+  console.log("%cDebug logs : " + message, 'color: lime');
+}
+
+function DebugError(message){
+  if(!DEBUG) return;
+  console.log("%cDebug error : " + message, 'color: red');
+}
+
+
+DebugLog("Debug mode is on");
